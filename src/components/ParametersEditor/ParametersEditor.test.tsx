3641db665b840d8d17a071e6f46ad809b2c5eef7
Alex Simonok <sashasimonok@gmail.com>
2023-06-01 12:32:37 +0300

    Fix code coverage

--- src/components/ParametersEditor/ParametersEditor.test.tsx
+++ src/components/ParametersEditor/ParametersEditor.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { act, screen, render, fireEvent } from '@testing-library/react';
 import { TestIds } from '../../constants';
 import { ParametersEditor } from './ParametersEditor';
 
@@ -8,14 +8,30 @@ import { ParametersEditor } from './ParametersEditor';
  */
 describe('Editor', () => {
   const model = { fields: [] };
+  const getComponent = ({ query = {}, ...restProps }: any) => {
+    return <ParametersEditor {...restProps} query={query} />;
+  };
 
   it('Should find component with Button', async () => {
-    const getComponent = ({ query = {}, ...restProps }: any) => {
-      return <ParametersEditor {...restProps} query={query} />;
-    };
-
     render(getComponent({ model }));
 
     expect(screen.getByTestId(TestIds.parametersEditor.buttonAdd)).toBeInTheDocument();
+  });
+
+  it('Should add new parameter', async () => {
+    const onChange = jest.fn();
+    const onRunQuery = jest.fn();
+    render(getComponent({ model, onChange, onRunQuery }));
+
+    await act(() => fireEvent.click(screen.getByTestId(TestIds.parametersEditor.buttonAdd)));
+
+    expect(onChange).toHaveBeenCalledWith(
+      expect.objectContaining({
+        params: expect.objectContaining({
+          '': '',
+        }),
+      })
+    );
+    expect(onRunQuery).toHaveBeenCalled();
   });
 });
